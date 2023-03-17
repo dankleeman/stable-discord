b227c132e0c2d3d2cc62105bdc45292b5e24c7ff
Daniel Kleeman <dan.kleeman.git@pm.me>
2023-03-16 19:30:10 -0500

    Adding first image generation capability. (#9)
    * Adding first image generation capability.
    
    * Running make format.

--- stable_discord/bot.py
+++ stable_discord/bot.py
@@ -3,23 +3,25 @@ import os
 
 import discord
 
-from stable_discord.diffuser import BaseDiffuser
+from stable_discord.diffuser import Diffuser
 from stable_discord.parser import PromptParser
 
 logger = logging.getLogger(__name__)
 
 
+# TODO: Look into unblocking some of these steps: https://stackoverflow.com/questions/65881761/discord-gateway-warning-shard-id-none-heartbeat-blocked-for-more-than-10-second
 class StableDiscordBot(discord.Client):
     # TODO: Standardize emojis
     ack_emoji = "\N{THUMBS UP SIGN}"
     in_prog_emoji = "\N{STOPWATCH}"
-    done_emoji = "✔"  # TODO: Pick a check mark that shows up green on discord
+    done_emoji = "💯"  # TODO: Pick a check mark that shows up green on discord
     prompt_parser = PromptParser()
-    diffuser = BaseDiffuser()
+    diffuser = Diffuser()
 
     def __init__(self, wake_word="/art", *, intents, **options):
         super().__init__(intents=intents, **options)
         self.wake_word = wake_word
+        self.seen_channels = []
 
     def clean_message(self, s):
         return s.lstrip().replace(self.wake_word, "")
@@ -40,13 +42,25 @@ class StableDiscordBot(discord.Client):
             await message.channel.send(f"Skipping unknown args: {unknown_args}")
 
         await message.add_reaction(self.in_prog_emoji)
-        await message.channel.send(file=discord.File("image.png"))
+        self.diffuser.make_image(**known_args)
+        await message.channel.send(file=discord.File("img.png"))
         await message.add_reaction(self.done_emoji)
         # TODO: Look up how to remove reactions
 
     async def on_ready(self):
         # TODO: Hook into discord logging
         logger.info("Logged on as %s!", self.user)
+        for guild in self.guilds:
+            for channel in guild.text_channels:
+                logger.info("Announcing login in channel: %s", channel)
+                self.seen_channels.append(channel)
+                await channel.send("I'm here!")
+
+    async def sign_off(self):
+        logger.info("Signing off.")
+        for channel in self.seen_channels:
+            logger.info("Signing off in channel: %s", channel)
+            await channel.send("I'm done for now.")
 
     async def on_message(self, message):
         logger.debug("Seen message '%s' from %s", message.content, message.author)
@@ -66,10 +80,15 @@ class StableDiscordBot(discord.Client):
             await self.process_prompt(message)
 
 
+# TODO: Fold this into the bot class
 def launch_bot(bot_client):
     intents = discord.Intents(value=68608)
     intents.message_content = True
     intents.messages = True
+    intents.guilds = True
 
     bot = bot_client(intents=intents)
-    bot.run(os.getenv("DISCORD_TOKEN"))
+    try:
+        bot.run(os.getenv("DISCORD_TOKEN"))
+    finally:
+        pass
--- stable_discord/diffuser.py
+++ stable_discord/diffuser.py
@@ -1,2 +1,19 @@
-class BaseDiffuser:
-    pass
+import logging
+
+import torch
+from diffusers import StableDiffusionPipeline
+
+logger = logging.getLogger(__name__)
+
+
+class Diffuser:
+    def __init__(self, model_name="stabilityai/stable-diffusion-2-1"):
+        logger.debug("CUDA is available: %s", torch.cuda.is_available())
+        self.cuda_is_available = torch.cuda.is_available()
+        self.pipeline = StableDiffusionPipeline.from_pretrained(model_name, torch_dtype=torch.float16)
+        self.pipeline.enable_attention_slicing()
+        self.pipeline.to("cuda")
+
+    def make_image(self, prompt, cfg, steps):
+        image = self.pipeline(prompt=prompt, guidance_scale=cfg, num_inference_steps=steps).images[0]
+        image.save("img.png")
